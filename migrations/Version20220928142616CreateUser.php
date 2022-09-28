7c81f79fb412374d370f025fe008bc0f5ab0b6ad
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-09-28 16:30:41 +0200

    Add a basic User entity
    This entity is only meant to let the migrations work, or the CI would
    fail otherwise. The User will be enhanced a bit later.

---
+++ migrations/Version20220928142616CreateUser.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220928142616CreateUser extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create the user table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
+        $this->addSql('DROP TABLE "user"');
+    }
+}
