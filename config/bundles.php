d98e253c2b4567488ee0fb82b4b4743e85579ef1
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-09-29 10:06:45 +0200

    tec: Enable Symfony Profiler in dev environment
    The FastCGI buffer size has to be increased for Nginx because the HTTP
    header is too big with the profiler enabled (error `upstream sent too
    big header while reading response header from upstream`).
    
    References:
    
    - http://nginx.org/en/docs/http/ngx_http_fastcgi_module.html
    - https://github.com/api-platform/api-platform/issues/890#issuecomment-440278103

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
