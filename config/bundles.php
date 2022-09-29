209526fafb468f1b8ae614bc57d8f6d763b51ec2
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-09-29 09:52:44 +0200

    tec: Add and configure Twig

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,14 +4,13 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'home', methods: ['GET', 'HEAD'])]
     public function show(): Response
     {
-        return new Response(
-            '<html><body><p>Hello ProbeSuite!</p></body></html>'
-        );
+        return $this->render('home/show.html.twig');
     }
 }
