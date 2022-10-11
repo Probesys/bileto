5805645d25e1ae0644cf864058008be3fc2e138a
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-10-11 16:49:58 +0200

    tec: Remove useless code in tests/bootstrap
    This should have been removed in 11509ce. Foundry does it for us in the
    `ResetDatabase` trait (while being less performant since it is done for
    each test file).

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,26 +13,3 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
-
-// Reset the database before running the tests
-$kernel = new \App\Kernel('test', true);
-$kernel->boot();
-
-$application = new Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
-$application->setAutoExit(false);
-$output = new \Symfony\Component\Console\Output\NullOutput();
-
-$commands = [
-    'doctrine:database:drop --force --if-exists',
-    'doctrine:database:create',
-    'doctrine:schema:create',
-];
-foreach ($commands as $command) {
-    $input = new \Symfony\Component\Console\Input\StringInput($command);
-    $result = $application->run($input, $output);
-    if ($result !== 0) {
-        exit($result);
-    }
-}
-
-$kernel->shutdown();
