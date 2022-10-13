1c4c3dedcba9719abd416b1d4937ecb3fb396cab
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-10-13 11:31:08 +0200

    misc: Rename login/index template into login/new

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/index.html.twig', [
+        return $this->render('login/new.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
