086469b6936bec9876fbdb42872d0706be9c2b1d
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-10-13 17:17:30 +0200

    fix: Render html lang attribute as BCP 47
    References:
    
    - https://developer.mozilla.org/docs/Web/HTML/Global_attributes/lang
    - https://www.ietf.org/rfc/bcp/bcp47.txt

---
+++ src/Twig/LocaleToBCP47Extension.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class LocaleToBCP47Extension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter(
+                'locale_to_bcp47',
+                [$this, 'localeToBCP47'],
+            ),
+        ];
+    }
+
+    public function localeToBCP47(string $locale): string
+    {
+        $splittedLocale = explode('_', $locale, 2);
+        if (count($splittedLocale) === 1) {
+            return $splittedLocale[0];
+        }
+
+        return $splittedLocale[0] . '-' . strtoupper($splittedLocale[1]);
+    }
+}
