c604a51898f7a538b82c84a1a5cfc6dce5b5eadf
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-10-13 09:19:51 +0200

    tec: Add Turbo and Stimulus

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,3 +1,8 @@
 // This file is part of Bileto.
 // Copyright 2022 Probesys
 // SPDX-License-Identifier: AGPL-3.0-or-later
+
+import * as Turbo from '@hotwired/turbo'; // eslint-disable-line no-unused-vars
+import { Application } from '@hotwired/stimulus';
+
+const application = Application.start(); // eslint-disable-line no-unused-vars
