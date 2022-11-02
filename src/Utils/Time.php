2dcd8a2e0d38dcb47e19f524830eb4bcf78685e0
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-11-02 11:46:36 +0100

    tec: Provide a Time class
    This class is useful to test operations based on the time. It must
    **always** be used instead of default `\DateTime` methods.

---
+++ src/Utils/Time.php
@@ -0,0 +1,61 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Utils;
+
+class Time
+{
+    /** @var \DateTimeImmutable|null $freezedNow */
+    private static $freezedNow;
+
+    public static function now(): \DateTimeImmutable
+    {
+        if (self::$freezedNow) {
+            return self::$freezedNow;
+        } else {
+            return new \DateTimeImmutable('now');
+        }
+    }
+
+    /**
+     * @see https://www.php.net/manual/datetime.modify.php
+     * @see https://www.php.net/manual/datetime.formats.relative.php
+     */
+    public static function relative(string $modifier): \DateTimeImmutable
+    {
+        return self::now()->modify($modifier);
+    }
+
+    /**
+     * Return a datetime from the future.
+     *
+     * @see https://www.php.net/manual/en/datetime.formats.relative.php
+     */
+    public static function fromNow(int $number, string $unit): \DateTimeImmutable
+    {
+        return self::relative("+{$number} {$unit}");
+    }
+
+    /**
+     * Return a datetime from the past.
+     *
+     * @see https://www.php.net/manual/en/datetime.formats.relative.php
+     */
+    public static function ago(int $number, string $unit): \DateTimeImmutable
+    {
+        return self::relative("-{$number} {$unit}");
+    }
+
+    public static function freeze(\DateTimeImmutable $datetime): void
+    {
+        self::$freezedNow = $datetime;
+    }
+
+    public static function unfreeze(): void
+    {
+        self::$freezedNow = null;
+    }
+}
---
+++ tests/Utils/TimeTest.php
@@ -0,0 +1,51 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Tests\Utils;
+
+use App\Utils\Time;
+use PHPUnit\Framework\TestCase;
+
+class TimeTest extends TestCase
+{
+    /**
+     * @beforeClass
+     */
+    public static function freezeTime(): void
+    {
+        $now = new \DateTimeImmutable('2022-11-02');
+        Time::freeze($now);
+    }
+
+    /**
+     * @afterClass
+     */
+    public static function unfreezeTime(): void
+    {
+        Time::unfreeze();
+    }
+
+    public function testNow(): void
+    {
+        $now = Time::now();
+
+        $this->assertEquals('2022-11-02', $now->format('Y-m-d'));
+    }
+
+    public function testFromNow(): void
+    {
+        $tomorrow = Time::fromNow(1, 'day');
+
+        $this->assertEquals('2022-11-03', $tomorrow->format('Y-m-d'));
+    }
+
+    public function testAgo(): void
+    {
+        $yesterday = Time::ago(1, 'day');
+
+        $this->assertEquals('2022-11-01', $yesterday->format('Y-m-d'));
+    }
+}
