bf38538e90e4b64aff96d0fcf59f7e8644a65793
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-11-22 09:34:00 +0100

    fix: Add UniqueEntity constraint on Ticket uid field

--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -11,10 +11,15 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Translation\TranslatableMessage;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TicketRepository::class)]
+#[UniqueEntity(
+    fields: 'uid',
+    message: new TranslatableMessage('The uid {{ value }} is already used.', [], 'validators'),
+)]
 class Ticket
 {
     public const TYPES = ['request', 'incident'];
