cae15eef4c1bc3a36a89f27e4df72c93518dcd5e
Marien Fressinaud <marien.fressinaud@probesys.com>
2022-11-25 15:50:01 +0100

    fix: Fix the name of the last migration class

--- migrations/Version20221123163702RenameMessageIsPrivateToIsConfidential.php
+++ migrations/Version20221123163702RenameMessageIsPrivateToIsConfidential.php
@@ -11,7 +11,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20221123163702 extends AbstractMigration
+final class Version20221123163702RenameMessageIsPrivateToIsConfidential extends AbstractMigration
 {
     public function getDescription(): string
     {
