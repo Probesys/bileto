530c3f028b7665600c29ec861bc2fdf20051fca4
Marien Fressinaud <marien.fressinaud@probesys.com>
2023-01-03 16:35:38 +0100

    imp: Allow to show / hide passwords

---
+++ assets/javascripts/controllers/password_controller.js
@@ -0,0 +1,33 @@
+// This file is part of Bileto.
+// Copyright 2022-2023 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static get targets () {
+        return ['input', 'button'];
+    }
+
+    connect () {
+        this.refreshButton();
+    }
+
+    refreshButton () {
+        if (this.inputTarget.type === 'password') {
+            this.buttonTarget.setAttribute('aria-pressed', false);
+        } else {
+            this.buttonTarget.setAttribute('aria-pressed', true);
+        }
+    }
+
+    toggle (e) {
+        if (this.inputTarget.type === 'password') {
+            this.inputTarget.type = 'text';
+        } else {
+            this.inputTarget.type = 'password';
+        }
+
+        this.refreshButton();
+    }
+};
