45bebc58be8a201335b3418176aa07c9f2a85370
Marien Fressinaud <marien.fressinaud@probesys.com>
2023-02-17 12:07:16 +0100

    imp: Improve format of ticket events

---
+++ src/Twig/TicketEventChangesFormatterExtension.php
@@ -0,0 +1,261 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2023 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Twig;
+
+use App\Entity\EntityEvent;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TicketEventChangesFormatterExtension extends AbstractExtension
+{
+    private UserRepository $userRepository;
+
+    private TranslatorInterface $translator;
+
+    public function __construct(UserRepository $userRepository, TranslatorInterface $translator)
+    {
+        $this->userRepository = $userRepository;
+        $this->translator = $translator;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('formatTicketChanges', [$this, 'formatTicketChanges']),
+        ];
+    }
+
+    public function formatTicketChanges(EntityEvent $event, string $field): string
+    {
+        $user = $event->getCreatedBy();
+        $changes = $event->getChanges();
+
+        if (!isset($changes[$field])) {
+            throw new \LogicException("Cannot format {$field} changes of the EntityEvent");
+        }
+
+        $fieldChanges = $changes[$field];
+
+        if ($field === 'title') {
+            return $this->formatTitleChanges($user, $fieldChanges);
+        } elseif ($field === 'status') {
+            return $this->formatStatusChanges($user, $fieldChanges);
+        } elseif ($field === 'type') {
+            return $this->formatTypeChanges($user, $fieldChanges);
+        } elseif ($field === 'impact' || $field === 'priority' || $field === 'urgency') {
+            return $this->formatPriorityChanges($user, $field, $fieldChanges);
+        } elseif ($field === 'assignee') {
+            return $this->formatAssigneeChanges($user, $fieldChanges);
+        } elseif ($field === 'requester') {
+            return $this->formatRequesterChanges($user, $fieldChanges);
+        } elseif ($field === 'solution') {
+            return $this->formatSolutionChanges($user, $fieldChanges);
+        } else {
+            return $this->formatChanges($user, $field, $fieldChanges);
+        }
+    }
+
+    /**
+     * @param string[] $changes
+     */
+    private function formatTitleChanges(User $user, array $changes): string
+    {
+        $username = $this->escape($user->getDisplayName());
+        $oldValue = $this->escape($changes[0] ?? '');
+        $newValue = $this->escape($changes[1] ?? '');
+
+        return $this->translator->trans(
+            'tickets.events.title',
+            [
+                'username' => $username,
+                'oldValue' => $oldValue,
+                'newValue' => $newValue,
+            ]
+        );
+    }
+
+    /**
+     * @param string[] $changes
+     */
+    private function formatStatusChanges(User $user, array $changes): string
+    {
+        $username = $this->escape($user->getDisplayName());
+        $oldValue = $this->translator->trans('tickets.status.' . $changes[0]);
+        $newValue = $this->translator->trans('tickets.status.' . $changes[1]);
+        return $this->translator->trans(
+            'tickets.events.status',
+            [
+                'username' => $username,
+                'oldValue' => $oldValue,
+                'newValue' => $newValue,
+            ]
+        );
+    }
+
+    /**
+     * @param string[] $changes
+     */
+    private function formatTypeChanges(User $user, array $changes): string
+    {
+        $username = $this->escape($user->getDisplayName());
+
+        if ($changes[1] === 'request') {
+            return $this->translator->trans(
+                'tickets.events.type.to_request',
+                ['username' => $username],
+            );
+        } elseif ($changes[1] === 'incident') {
+            return $this->translator->trans(
+                'tickets.events.type.to_incident',
+                ['username' => $username],
+            );
+        } else {
+            throw new \LogicException("Ticket event with type change contains an invalid value ({$changes[1]})");
+        }
+    }
+
+    /**
+     * @param string[] $changes
+     */
+    private function formatPriorityChanges(User $user, string $field, array $changes): string
+    {
+        $parameters = [
+            'username' => $this->escape($user->getDisplayName()),
+            'oldValue' => $this->translator->trans("tickets.{$field}.{$changes[0]}"),
+            'newValue' => $this->translator->trans("tickets.{$field}.{$changes[1]}"),
+        ];
+
+        // We can't build the key dynamically, or the extractor would delete the translations.
+        if ($field === 'impact') {
+            return $this->translator->trans('tickets.events.impact', $parameters);
+        } elseif ($field === 'priority') {
+            return $this->translator->trans('tickets.events.priority', $parameters);
+        } elseif ($field === 'urgency') {
+            return $this->translator->trans('tickets.events.urgency', $parameters);
+        } else {
+            throw new \LogicException("formatPriorityChanges cannot be called with {$field} field");
+        }
+    }
+
+    /**
+     * @param array<?int> $changes
+     */
+    private function formatAssigneeChanges(User $user, array $changes): string
+    {
+        $username = $this->escape($user->getDisplayName());
+
+        if ($changes[0] === null) {
+            $newAssignee = $this->userRepository->find($changes[1]);
+            $newAssigneeUsername = $this->escape($newAssignee->getDisplayName());
+            return $this->translator->trans(
+                'tickets.events.assignee.assigned',
+                [
+                    'username' => $username,
+                    'newValue' => $newAssigneeUsername,
+                ]
+            );
+        } elseif ($changes[1] === null) {
+            $oldAssignee = $this->userRepository->find($changes[0]);
+            $oldAssigneeUsername = $this->escape($oldAssignee->getDisplayName());
+            return $this->translator->trans(
+                'tickets.events.assignee.unassigned',
+                [
+                    'username' => $username,
+                    'oldValue' => $oldAssigneeUsername,
+                ]
+            );
+        } else {
+            $oldAssignee = $this->userRepository->find($changes[0]);
+            $oldAssigneeUsername = $this->escape($oldAssignee->getDisplayName());
+            $newAssignee = $this->userRepository->find($changes[1]);
+            $newAssigneeUsername = $this->escape($newAssignee->getDisplayName());
+            return $this->translator->trans(
+                'tickets.events.assignee.changed',
+                [
+                    'username' => $username,
+                    'newValue' => $newAssigneeUsername,
+                    'oldValue' => $oldAssigneeUsername,
+                ]
+            );
+        }
+    }
+
+    /**
+     * @param int[] $changes
+     */
+    private function formatRequesterChanges(User $user, array $changes): string
+    {
+        $username = $this->escape($user->getDisplayName());
+        $oldRequester = $this->userRepository->find($changes[0]);
+        $oldRequesterUsername = $this->escape($oldRequester->getDisplayName());
+        $newRequester = $this->userRepository->find($changes[1]);
+        $newRequesterUsername = $this->escape($newRequester->getDisplayName());
+
+        return $this->translator->trans(
+            'tickets.events.requester',
+            [
+                'username' => $username,
+                'newValue' => $newRequesterUsername,
+                'oldValue' => $oldRequesterUsername,
+            ]
+        );
+    }
+
+    /**
+     * @param array<?int> $changes
+     */
+    private function formatSolutionChanges(User $user, array $changes): string
+    {
+        $username = $this->escape($user->getDisplayName());
+
+        if ($changes[0] === null) {
+            return $this->translator->trans(
+                'tickets.events.solution.new',
+                ['username' => $username],
+            );
+        } elseif ($changes[1] === null) {
+            return $this->translator->trans(
+                'tickets.events.solution.removed',
+                ['username' => $username],
+            );
+        } else {
+            return $this->translator->trans(
+                'tickets.events.solution.changed',
+                ['username' => $username],
+            );
+        }
+    }
+
+    /**
+     * @param mixed[] $changes
+     */
+    private function formatChanges(User $user, string $field, array $changes): string
+    {
+        $parameters = [
+            'username' => $this->escape($user->getDisplayName()),
+            'field' => $field,
+            'oldValue' => $this->escape($changes[0] ?? ''),
+            'newValue' => $this->escape($changes[1] ?? ''),
+        ];
+
+        if ($changes[0] === null) {
+            return $this->translator->trans('tickets.events.default.new', $parameters);
+        } elseif ($changes[1] === null) {
+            return $this->translator->trans('tickets.events.default.removed', $parameters);
+        } else {
+            return $this->translator->trans('tickets.events.default.changed', $parameters);
+        }
+    }
+
+    private function escape(string $string): string
+    {
+        return htmlspecialchars($string, \ENT_QUOTES | \ENT_SUBSTITUTE, 'UTF-8');
+    }
+}
---
+++ tests/Twig/TicketEventChangesFormatterExtensionTest.php
@@ -0,0 +1,288 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2023 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Tests\Twig;
+
+use App\Entity\EntityEvent;
+use App\Entity\Ticket;
+use App\Repository\EntityEventRepository;
+use App\Repository\TicketRepository;
+use App\Tests\Factory\MessageFactory;
+use App\Tests\Factory\OrganizationFactory;
+use App\Tests\Factory\TicketFactory;
+use App\Tests\Factory\UserFactory;
+use App\Twig\TicketEventChangesFormatterExtension;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class TicketEventChangesFormatterExtensionTest extends WebTestCase
+{
+    use Factories;
+    use ResetDatabase;
+
+    private KernelBrowser $client;
+
+    private TicketRepository $ticketRepository;
+
+    private EntityEventRepository $entityEventRepository;
+
+    private TicketEventChangesFormatterExtension $formatter;
+
+    /**
+     * @before
+     */
+    public function setUpTest(): void
+    {
+        $this->client = static::createClient();
+
+        $container = static::getContainer();
+        /** @var \Doctrine\Bundle\DoctrineBundle\Registry $registry */
+        $registry = $container->get('doctrine');
+        $entityManager = $registry->getManager();
+
+        /** @var TicketRepository $repository */
+        $repository = $entityManager->getRepository(Ticket::class);
+        $this->ticketRepository = $repository;
+
+        /** @var EntityEventRepository $repository */
+        $repository = $entityManager->getRepository(EntityEvent::class);
+        $this->entityEventRepository = $repository;
+
+        /** @var TIcketEventChangesFormatterExtension $formatter */
+        $formatter = $container->get(TicketEventChangesFormatterExtension::class);
+        $this->formatter = $formatter;
+
+        $user = UserFactory::createOne();
+        $this->client->loginUser($user->object());
+    }
+
+    /**
+     * @param Proxy<Ticket> $ticket
+     */
+    private function saveEvent(Proxy $ticket): EntityEvent
+    {
+        $this->ticketRepository->save($ticket->object(), true);
+        return $this->entityEventRepository->findOneBy([
+            'type' => 'update',
+            'entityType' => Ticket::class,
+            'entityId' => $ticket->getId(),
+        ]);
+    }
+
+    public function testFormatTicketChangesFormatsTitle(): void
+    {
+        $ticket = TicketFactory::createOne([
+            'title' => 'The old title',
+        ]);
+        $ticket->setTitle('The new title');
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'title');
+
+        $this->assertStringContainsString('renamed the ticket', $message);
+    }
+
+    public function testFormatTicketChangesFormatsStatus(): void
+    {
+        $ticket = TicketFactory::createOne([
+            'status' => 'new',
+        ]);
+        $ticket->setStatus('in_progress');
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'status');
+
+        $this->assertStringContainsString('changed the status', $message);
+    }
+
+    public function testFormatTicketChangesFormatsTypeToRequest(): void
+    {
+        $ticket = TicketFactory::createOne([
+            'type' => 'incident',
+        ]);
+        $ticket->setType('request');
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'type');
+
+        $this->assertStringContainsString('changed the ticket to request', $message);
+    }
+
+    public function testFormatTicketChangesFormatsTypeToIncident(): void
+    {
+        $ticket = TicketFactory::createOne([
+            'type' => 'request',
+        ]);
+        $ticket->setType('incident');
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'type');
+
+        $this->assertStringContainsString('changed the ticket to incident', $message);
+    }
+
+    public function testFormatTicketChangesFormatsImpact(): void
+    {
+        $ticket = TicketFactory::createOne([
+            'impact' => 'low',
+        ]);
+        $ticket->setImpact('medium');
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'impact');
+
+        $this->assertStringContainsString('changed the impact', $message);
+    }
+
+    public function testFormatTicketChangesFormatsPriority(): void
+    {
+        $ticket = TicketFactory::createOne([
+            'priority' => 'medium',
+        ]);
+        $ticket->setPriority('high');
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'priority');
+
+        $this->assertStringContainsString('changed the priority', $message);
+    }
+
+    public function testFormatTicketChangesFormatsUrgency(): void
+    {
+        $ticket = TicketFactory::createOne([
+            'urgency' => 'high',
+        ]);
+        $ticket->setUrgency('low');
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'urgency');
+
+        $this->assertStringContainsString('changed the urgency', $message);
+    }
+
+    public function testFormatTicketChangesFormatsAssigneeWhenAssigned(): void
+    {
+        $assignee = UserFactory::createOne();
+        $ticket = TicketFactory::createOne([
+            'assignee' => null,
+        ]);
+        $ticket->setAssignee($assignee->object());
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'assignee');
+
+        $this->assertStringContainsString('assigned the ticket', $message);
+    }
+
+    public function testFormatTicketChangesFormatsAssigneeWhenUnassigned(): void
+    {
+        $assignee = UserFactory::createOne();
+        $ticket = TicketFactory::createOne([
+            'assignee' => $assignee,
+        ]);
+        $ticket->setAssignee(null);
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'assignee');
+
+        $this->assertStringContainsString('removed the assignee', $message);
+    }
+
+    public function testFormatTicketChangesFormatsAssigneeWhenChanged(): void
+    {
+        $oldAssignee = UserFactory::createOne();
+        $newAssignee = UserFactory::createOne();
+        $ticket = TicketFactory::createOne([
+            'assignee' => $oldAssignee,
+        ]);
+        $ticket->setAssignee($newAssignee->object());
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'assignee');
+
+        $this->assertStringContainsString('changed the assignee', $message);
+    }
+
+    public function testFormatTicketChangesFormatsRequester(): void
+    {
+        $oldRequester = UserFactory::createOne();
+        $newRequester = UserFactory::createOne();
+        $ticket = TicketFactory::createOne([
+            'requester' => $oldRequester,
+        ]);
+        $ticket->setRequester($newRequester->object());
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'requester');
+
+        $this->assertStringContainsString('changed the requester', $message);
+    }
+
+    public function testFormatTicketChangesFormatsSolutionWhenNew(): void
+    {
+        $solution = MessageFactory::createOne();
+        $ticket = TicketFactory::createOne([
+            'solution' => null,
+        ]);
+        $ticket->setSolution($solution->object());
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'solution');
+
+        $this->assertStringContainsString('added a solution', $message);
+    }
+
+    public function testFormatTicketChangesFormatsSolutionWhenRemoved(): void
+    {
+        $solution = MessageFactory::createOne();
+        $ticket = TicketFactory::createOne([
+            'solution' => $solution,
+        ]);
+        $ticket->setSolution(null);
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'solution');
+
+        $this->assertStringContainsString('removed the solution', $message);
+    }
+
+    public function testFormatTicketChangesFormatsSolutionWhenChanged(): void
+    {
+        $oldSolution = MessageFactory::createOne();
+        $newSolution = MessageFactory::createOne();
+        $ticket = TicketFactory::createOne([
+            'solution' => $oldSolution,
+        ]);
+        $ticket->setSolution($newSolution->object());
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'solution');
+
+        $this->assertStringContainsString('changed the solution', $message);
+    }
+
+    public function testFormatTicketChangesFormatsWithDefaultMessageWhenChanged(): void
+    {
+        $oldOrganization = OrganizationFactory::createOne();
+        $newOrganization = OrganizationFactory::createOne();
+        $ticket = TicketFactory::createOne([
+            'organization' => $oldOrganization,
+        ]);
+        $ticket->setOrganization($newOrganization->object());
+        $event = $this->saveEvent($ticket);
+
+        $message = $this->formatter->formatTicketChanges($event, 'organization');
+
+        $message = strip_tags($message);
+        $this->assertStringContainsString(
+            "changed the organization from {$oldOrganization->getId()} to {$newOrganization->getId()}",
+            $message
+        );
+    }
+}
