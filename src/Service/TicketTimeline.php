6f4a6a7bfb8bcf60ec467917b83039919da8f5e9
Marien Fressinaud <marien.fressinaud@probesys.com>
2023-02-27 15:00:46 +0100

    tec: Extract a Timeline object

--- src/Service/TicketTimeline.php
+++ src/Service/TicketTimeline.php
@@ -6,10 +6,9 @@
 
 namespace App\Service;
 
-use App\Entity\EntityEvent;
-use App\Entity\Message;
 use App\Entity\Ticket;
 use App\Repository\EntityEventRepository;
+use App\Utils\Timeline;
 use Symfony\Bundle\SecurityBundle\Security;
 
 class TicketTimeline
@@ -26,11 +25,10 @@ class TicketTimeline
         $this->security = $security;
     }
 
-    /**
-     * @return array<Message|EntityEvent>
-     */
-    public function build(Ticket $ticket): array
+    public function build(Ticket $ticket): Timeline
     {
+        $timeline = new Timeline();
+
         $organization = $ticket->getOrganization();
         $includeConfidential = $this->security->isGranted(
             'orga:see:tickets:messages:confidential',
@@ -48,25 +46,9 @@ class TicketTimeline
             'type' => 'update',
         ]);
 
-        $items = array_merge($messages, $events);
+        $timeline->addItems($messages);
+        $timeline->addItems($events);
 
-        uasort($items, function ($i1, $i2) {
-            $createdAt1 = $i1->getCreatedAt();
-            $createdAt2 = $i2->getCreatedAt();
-
-            if ($createdAt1 < $createdAt2) {
-                return -1;
-            } elseif ($createdAt1 > $createdAt2) {
-                return 1;
-            } elseif ($i1->getTimelineType() === 'message') {
-                return -1;
-            } elseif ($i1->getTimelineType() === 'event') {
-                return 1;
-            } else {
-                return 0;
-            }
-        });
-
-        return $items;
+        return $timeline;
     }
 }
---
+++ src/Utils/Timeline.php
@@ -0,0 +1,66 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2023 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Utils;
+
+use App\Entity\EntityEvent;
+use App\Entity\Message;
+
+class Timeline
+{
+    /** @var array<string, array<Message|EntityEvent>> $items */
+    private array $items = [];
+
+    /**
+     * @param array<Message|EntityEvent> $items
+     */
+    public function addItems(array $items): void
+    {
+        foreach ($items as $item) {
+            $type = $item->getTimelineType();
+            $this->items[$type][] = $item;
+        }
+    }
+
+    /**
+     * @return array<Message|EntityEvent>
+     */
+    public function getItems(?string $type = null): array
+    {
+        if ($type) {
+            return $this->items[$type] ?? [];
+        } else {
+            return array_merge(...array_values($this->items));
+        }
+    }
+
+    /**
+     * @return array<Message|EntityEvent>
+     */
+    public function getSortedItems(?string $type = null): array
+    {
+        $items = $this->getItems($type);
+
+        uasort($items, function ($i1, $i2) {
+            $createdAt1 = $i1->getCreatedAt();
+            $createdAt2 = $i2->getCreatedAt();
+
+            if ($createdAt1 < $createdAt2) {
+                return -1;
+            } elseif ($createdAt1 > $createdAt2) {
+                return 1;
+            } elseif ($i1->getTimelineType() === 'message') {
+                return -1;
+            } elseif ($i1->getTimelineType() === 'event') {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return $items;
+    }
+}
