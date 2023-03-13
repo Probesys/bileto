c16932b6693b89e5190283b8fafb81f26e0ae724
Marien Fressinaud <marien.fressinaud@probesys.com>
2023-03-13 17:08:14 +0100

    new: Allow to scroll to the top of the page

---
+++ assets/javascripts/controllers/scroll_to_controller.js
@@ -0,0 +1,32 @@
+// This file is part of Bileto.
+// Copyright 2022-2023 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static get values () {
+        return {
+            target: String,
+        };
+    }
+
+    connect () {
+        if ('visible' in this.element.dataset) {
+            const intersectionObserver = new IntersectionObserver((entries) => {
+                entries.forEach((entry) => {
+                    entry.target.dataset.visible = !entry.isIntersecting;
+                });
+            });
+            intersectionObserver.observe(this.element);
+        }
+    }
+
+    scroll () {
+        const target = document.querySelector(this.targetValue);
+        if (target) {
+            const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+            target.scrollIntoView({ behavior: mediaQuery.matches ? 'auto' : 'smooth' });
+        }
+    }
+}
