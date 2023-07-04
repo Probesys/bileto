36b6b26b4c372b290a95dfc95d08d772f7e080cb
Marien Fressinaud <marien.fressinaud@probesys.com>
2023-07-04 14:01:42 +0200

    Restrict list of actors to the user authorizations

--- src/Service/ActorsLister.php
+++ src/Service/ActorsLister.php
@@ -8,6 +8,8 @@ namespace App\Service;
 
 use App\Entity\Organization;
 use App\Entity\User;
+use App\Repository\AuthorizationRepository;
+use App\Repository\OrganizationRepository;
 use App\Repository\UserRepository;
 use App\Service\UserSorter;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -15,6 +17,8 @@ use Symfony\Bundle\SecurityBundle\Security;
 class ActorsLister
 {
     public function __construct(
+        private AuthorizationRepository $authRepository,
+        private OrganizationRepository $orgaRepository,
         private UserRepository $userRepository,
         private UserSorter $userSorter,
         private Security $security,
@@ -28,8 +32,12 @@ class ActorsLister
     {
         $currentUser = $this->security->getUser();
 
+        $authorizedOrgaIds = $this->findAuthorizedOrganizationIds($currentUser);
+
         $orgaIds = $organization->getParentOrganizationIds();
         $orgaIds[] = $organization->getId();
+
+        $orgaIds = array_intersect($authorizedOrgaIds, $orgaIds);
 
         $users = $this->userRepository->findByOrganizationIds($orgaIds);
 
@@ -53,7 +61,9 @@ class ActorsLister
     {
         $currentUser = $this->security->getUser();
 
-        $users = $this->userRepository->findAll();
+        $authorizedOrgaIds = $this->findAuthorizedOrganizationIds($currentUser);
+
+        $users = $this->userRepository->findByOrganizationIds($authorizedOrgaIds);
 
         $this->userSorter->sort($users);
 
@@ -66,5 +76,22 @@ class ActorsLister
         } else {
             return $users;
         }
+    }
+
+    /**
+     * @return int[]
+     */
+    private function findAuthorizedOrganizationIds(User $currentUser): array
+    {
+        $orgaIds = $this->authRepository->getAuthorizedOrganizationIds($currentUser);
+        if (in_array(null, $orgaIds)) {
+            $organizations = $this->orgaRepository->findAll();
+        } else {
+            $organizations = $this->orgaRepository->findWithSubOrganizations($orgaIds);
+        }
+
+        return array_map(function ($orga) {
+            return $orga->getId();
+        }, $organizations);
     }
 }
