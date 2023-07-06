d8a3c6ef20e8f19cacde69e5c786f69601b06f11
Marien Fressinaud <marien.fressinaud@probesys.com>
2023-07-06 09:41:44 +0200

    new: Allow to delete a mailbox email in error

---
+++ src/Controller/MailboxEmailsController.php
@@ -0,0 +1,39 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2023 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Controller;
+
+use App\Entity\MailboxEmail;
+use App\Repository\MailboxEmailRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class MailboxEmailsController extends BaseController
+{
+    #[Route('/mailbox-emails/{uid}/deletion', name: 'delete mailbox email', methods: ['POST'])]
+    public function delete(
+        MailboxEmail $mailboxEmail,
+        Request $request,
+        MailboxEmailRepository $mailboxEmailRepository,
+        TranslatorInterface $translator,
+    ): Response {
+        $this->denyAccessUnlessGranted('admin:manage:mailboxes');
+
+        /** @var string $csrfToken */
+        $csrfToken = $request->request->get('_csrf_token', '');
+
+        if (!$this->isCsrfTokenValid('delete mailbox email', $csrfToken)) {
+            $this->addFlash('error', $translator->trans('csrf.invalid', [], 'errors'));
+            return $this->redirectToRoute('mailboxes');
+        }
+
+        $mailboxEmailRepository->remove($mailboxEmail, true);
+
+        return $this->redirectToRoute('mailboxes');
+    }
+}
---
+++ tests/Controller/MailboxEmailsControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2023 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Tests\Controller;
+
+use App\Tests\AuthorizationHelper;
+use App\Tests\Factory\MailboxEmailFactory;
+use App\Tests\Factory\UserFactory;
+use App\Tests\SessionHelper;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class MailboxEmailsControllerTest extends WebTestCase
+{
+    use AuthorizationHelper;
+    use Factories;
+    use ResetDatabase;
+    use SessionHelper;
+
+    public function testPostDeleteRemovesTheMailboxEmailAndRedirects(): void
+    {
+        $client = static::createClient();
+        $user = UserFactory::createOne();
+        $client->loginUser($user->object());
+        $this->grantAdmin($user->object(), ['admin:manage:mailboxes']);
+        $mailboxEmail = MailboxEmailFactory::createOne();
+
+        $client->request('POST', "/mailbox-emails/{$mailboxEmail->getUid()}/deletion", [
+            '_csrf_token' => $this->generateCsrfToken($client, 'delete mailbox email'),
+        ]);
+
+        $this->assertResponseRedirects('/mailboxes', 302);
+        MailboxEmailFactory::assert()->notExists(['id' => $mailboxEmail->getId()]);
+    }
+
+    public function testPostDeleteFailsIfCsrfTokenIsInvalid(): void
+    {
+        $client = static::createClient();
+        $user = UserFactory::createOne();
+        $client->loginUser($user->object());
+        $this->grantAdmin($user->object(), ['admin:manage:mailboxes']);
+        $mailboxEmail = MailboxEmailFactory::createOne();
+
+        $client->request('POST', "/mailbox-emails/{$mailboxEmail->getUid()}/deletion", [
+            '_csrf_token' => 'not the token',
+        ]);
+
+        $this->assertResponseRedirects('/mailboxes', 302);
+        $client->followRedirect();
+        $this->assertSelectorTextContains('#notifications', 'The security token is invalid');
+        MailboxEmailFactory::assert()->exists(['id' => $mailboxEmail->getId()]);
+    }
+
+    public function testPostDeleteFailsIfAccessIsForbidden(): void
+    {
+        $this->expectException(AccessDeniedException::class);
+
+        $client = static::createClient();
+        $user = UserFactory::createOne();
+        $client->loginUser($user->object());
+        $mailboxEmail = MailboxEmailFactory::createOne();
+
+        $client->catchExceptions(false);
+        $client->request('POST', "/mailbox-emails/{$mailboxEmail->getUid()}/deletion", [
+            '_csrf_token' => $this->generateCsrfToken($client, 'delete mailbox email'),
+        ]);
+    }
+}
