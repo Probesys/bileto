be20bb0d90b93e4934e8860a382bc629956ea995
Marien Fressinaud <marien.fressinaud@probesys.com>
2023-08-29 16:13:55 +0200

    imp: Allow to upload any kind of files

--- assets/javascripts/controllers/message_documents_controller.js
+++ assets/javascripts/controllers/message_documents_controller.js
@@ -5,6 +5,10 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    static get targets () {
+        return ['uploadButton', 'filesSelector'];
+    }
+
     removeDocument (event) {
         const button = event.target.closest('button');
 
@@ -29,5 +33,48 @@ export default class extends Controller {
 
     reload () {
         this.element.reload();
+    }
+
+    openFilesSelector () {
+        this.filesSelectorTarget.click();
+    }
+
+    uploadFiles () {
+        this.uploadButtonTarget.disabled = true;
+
+        const file = this.filesSelectorTarget.files[0];
+        const uploadUrl = this.filesSelectorTarget.getAttribute('data-upload-url');
+        const uploadCsrf = this.filesSelectorTarget.getAttribute('data-upload-csrf');
+
+        const formData = new FormData();
+        formData.append('document', file, file.name);
+        formData.append('_csrf_token', uploadCsrf);
+
+        fetch(uploadUrl, {
+            method: 'POST',
+            body: formData,
+        }).then((response) => {
+            return response.json();
+        }).then((json) => {
+            if (json.uid != null) {
+                const newDocumentEvent = new CustomEvent('new-document', { detail: json });
+                this.element.dispatchEvent(newDocumentEvent);
+
+                this.reload();
+            } else {
+                alert(json.error);
+
+                if (json.description) {
+                    console.error(json.description);
+                }
+            }
+
+            this.uploadButtonTarget.disabled = false;
+        }).catch((error) => {
+            alert('Bad response from the server.');
+            console.error(error);
+
+            this.uploadButtonTarget.disabled = false;
+        });
     }
 }
