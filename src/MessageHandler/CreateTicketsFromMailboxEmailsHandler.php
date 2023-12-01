Probesys/bileto

commit 417a76d574de5de783af9ce70af93c1e73cfc559

fix: Set correct inline attachments URLs in messages contents

src/MessageHandler/CreateTicketsFromMailboxEmailsHandler.php
@@ -28,6 +28,7 @@ use Symfony\Component\HtmlSanitizer\HtmlSanitizerInterface;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
 use Webklex\PHPIMAP;
 
@@ -46,6 +47,7 @@ class CreateTicketsFromMailboxEmailsHandler
         private HtmlSanitizerInterface $appMessageSanitizer,
         private MessageBusInterface $bus,
         private LoggerInterface $logger,
+        private UrlGeneratorInterface $urlGenerator,
     ) {
     }
 
@@ -122,11 +124,9 @@ class CreateTicketsFromMailboxEmailsHandler
                 }
             }
 
-            $messageContent = $this->appMessageSanitizer->sanitize($mailboxEmail->getBody());
-
             $message = new Message();
             $message->setCreatedBy($requester);
-            $message->setContent($messageContent);
+            $message->setContent(''); // this is set below
             $message->setTicket($ticket);
             $message->setIsConfidential(false);
             $message->setVia('email');
@@ -142,6 +142,17 @@ class CreateTicketsFromMailboxEmailsHandler
             }
 
             $this->messageDocumentRepository->saveBatch($messageDocuments, true);
+
+            $messageContent = $mailboxEmail->getBody();
+            // Inline attachments (i.e. <img>) have URLs of type: `cid:<id>`.
+            // Here we replace these URLs with the application URLs to the message documents.
+            $messageContent = $this->replaceAttachmentsUrls($messageContent, $messageDocuments);
+            // Sanitize the HTML only after replacing the URLs or it would
+            // remove the `src` attributes as the `cid:` scheme is forbidden.
+            $messageContent = $this->appMessageSanitizer->sanitize($messageContent);
+
+            $message->setContent($messageContent);
+            $this->messageRepository->save($message, true);
 
             $this->mailboxEmailRepository->remove($mailboxEmail, true);
 
@@ -209,6 +220,51 @@ class CreateTicketsFromMailboxEmailsHandler
         }
 
         return $messageDocuments;
+    }
+
+    /**
+     * Replace (in the given content) the image src attributes of the inline
+     * attachments with the URLs of the corresponding message documents.
+     *
+     * @param array<string, MessageDocument> $messageDocuments
+     */
+    private function replaceAttachmentsUrls(string $content, array $messageDocuments): string
+    {
+        $contentDom = new \DOMDocument();
+        $contentDom->loadHTML($content);
+        $contentDomXPath = new \DomXPath($contentDom);
+
+        foreach ($messageDocuments as $attachmentId => $messageDocument) {
+            $imageNodes = $contentDomXPath->query("//img[@src='cid:{$attachmentId}']");
+
+            if ($imageNodes === false || $imageNodes->length === 0) {
+                // no corresponding node, the document was probably not inlined
+                continue;
+            }
+
+            $messageDocumentUrl = $this->urlGenerator->generate(
+                'message document',
+                [
+                    'uid' => $messageDocument->getUid(),
+                    'extension' => $messageDocument->getExtension(),
+                ],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            );
+
+            foreach ($imageNodes as $imageNode) {
+                if ($imageNode instanceof \DOMElement) {
+                    $imageNode->setAttribute('src', $messageDocumentUrl);
+                }
+            }
+        }
+
+        $result = $contentDom->saveHTML();
+
+        if ($result === false) {
+            return $content;
+        }
+
+        return $result;
     }
 
     private function markError(MailboxEmail $mailboxEmail, string $error): void

commit 22ef430892c9797b9d129e95736452674d1079e6

fix: Fix encoding of the email body
DOMDocument::loadHTML loads content as ISO-8859-1 by default. It means
that our UTF-8 $content variable is not correctly interpreted.

We could tell loadHTML to use UTF8, but we would need to alter the
content (e.g. by adding an XML encoding declaration). We cannot be sure
of how the string looks except by inspecting its content and I feel that
being a rabbit hole of problems.

The alternative is to convert all the non-ASCII characters to HTML
entities with some weird `mb_encode_numericentity` function. In my
experience, it works pretty well.

Another alternative that I didn't choose would be to use a better
library to parse HTML.

Bug introduced in: 417a76d574de5de783af9ce70af93c1e73cfc559

References:

- loadHTML using ISO-8859-1 by default https://stackoverflow.com/a/8218649
- about the `mb_encode_numericentity` cryptic arguments https://stackoverflow.com/a/35858944
- HTML5 parser (no longer maintained?) https://github.com/Masterminds/html5-php

src/MessageHandler/CreateTicketsFromMailboxEmailsHandler.php
@@ -231,6 +231,17 @@ class CreateTicketsFromMailboxEmailsHandler
     private function replaceAttachmentsUrls(string $content, array $messageDocuments): string
     {
         $contentDom = new \DOMDocument();
+
+        // DOMDocument::loadHTML considers the source string to be encoded in
+        // ISO-8859-1 by default. In order to not ending with weird characters,
+        // we encode the non-ASCII chars (i.e. all chars above >0x80) to HTML
+        // entities.
+        $content = mb_encode_numericentity(
+            $content,
+            [0x80, 0x10FFFF, 0, -1],
+            'UTF-8'
+        );
+
         $contentDom->loadHTML($content);
         $contentDomXPath = new \DomXPath($contentDom);
 