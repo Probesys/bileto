242d0655f3725f36fbbcd98ee03f10d7f68c1db4
Marien Fressinaud <marien.fressinaud@probesys.com>
2023-12-20 09:38:37 +0100

    misc: Migrate the entity_event changes for MariaDB
    I don't understand this migration for MariaDB, but I guess it's
    important. That's something that I dislike with this kind of tool: weird
    things can happen, not letting you understand easily what's going on.
    
    The issue doesn't happen with PostgreSQL. I think it was a bug in
    Doctrine that has been fixed during the last update of the dependencies.

---
+++ migrations/Version20231220083507AlterEntityEventChanges.php
@@ -0,0 +1,37 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2023 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Platforms\MariaDBPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231220083507AlterEntityEventChanges extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Alter the column changes of the table entity_event.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $dbPlatform = $this->connection->getDatabasePlatform();
+        if ($dbPlatform instanceof MariaDBPlatform) {
+            $this->addSql('ALTER TABLE entity_event CHANGE changes changes JSON NOT NULL COMMENT \'(DC2Type:json)\'');
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        $dbPlatform = $this->connection->getDatabasePlatform();
+        if ($dbPlatform instanceof MariaDBPlatform) {
+            $this->addSql('ALTER TABLE entity_event CHANGE changes changes JSON NOT NULL COMMENT \'(DC2Type:json)\'');
+        }
+    }
+}
