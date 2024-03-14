3e1fffb948dccec51312a46580e56e45e1f82801
Marien Fressinaud <marien.fressinaud@probesys.com>
2024-03-14 15:05:38 +0100

    dev: Provide an ArrayHelper class

---
+++ src/Utils/ArrayHelper.php
@@ -0,0 +1,53 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2024 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Utils;
+
+/**
+ * Provide helpful methods to manipulate arrays.
+ */
+class ArrayHelper
+{
+    /**
+     * Find in an array and return (if any) the element matching the callback.
+     *
+     * @template TElement of mixed
+     *
+     * @param TElement[] $array
+     * @param callable(TElement): bool $callback
+     *
+     * @return ?TElement
+     */
+    public static function find(array $array, callable $callback): mixed
+    {
+        foreach ($array as $item) {
+            if ($callback($item)) {
+                return $item;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Return whether at least one element in the array matches the provided callback.
+     *
+     * @template TElement of mixed
+     *
+     * @param TElement[] $array
+     * @param callable(TElement): bool $callback
+     */
+    public static function any(array $array, callable $callback): bool
+    {
+        foreach ($array as $item) {
+            if ($callback($item)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
