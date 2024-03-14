d465d4b0100383b5dcd2967c3c7a00f6c81fee53
Marien Fressinaud <marien.fressinaud@probesys.com>
2024-03-14 16:29:51 +0100

    dev: Provide AuthorizationHelper::grantTeam

--- tests/AuthorizationHelper.php
+++ tests/AuthorizationHelper.php
@@ -9,7 +9,9 @@ namespace App\Tests;
 use App\Entity\Authorization;
 use App\Entity\Organization;
 use App\Entity\Role;
+use App\Entity\Team;
 use App\Entity\User;
+use App\Service\TeamService;
 use App\Utils\Random;
 use App\Utils\Time;
 use Doctrine\ORM\EntityManager;
@@ -87,5 +89,39 @@ trait AuthorizationHelper
 
         $roleRepo->save($role);
         $authorizationRepo->grant($user, $role, $organization);
+    }
+
+    /**
+     * @param string[] $permissions
+     */
+    public function grantTeam(
+        Team $team,
+        array $permissions,
+        ?Organization $organization = null,
+    ): void {
+        if (empty($permissions)) {
+            return;
+        }
+
+        $container = static::getContainer();
+        /** @var \Doctrine\Bundle\DoctrineBundle\Registry $registry */
+        $registry = $container->get('doctrine');
+        $entityManager = $registry->getManager();
+
+        /** @var \App\Repository\RoleRepository */
+        $roleRepo = $entityManager->getRepository(Role::class);
+        /** @var TeamService */
+        $teamService = $container->get(TeamService::class);
+
+        $permissions = Role::sanitizePermissions('agent', $permissions);
+
+        $role = new Role();
+        $role->setName(Random::hex(10));
+        $role->setDescription('The role description');
+        $role->setType('agent');
+        $role->setPermissions($permissions);
+
+        $roleRepo->save($role);
+        $teamService->createAuthorization($team, $role, $organization);
     }
 }
