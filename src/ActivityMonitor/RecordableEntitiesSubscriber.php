6c0cf9749de1d9c96ff11f1433b63b16eb0b23a8
Marien Fressinaud <marien.fressinaud@probesys.com>
2024-08-29 16:59:17 +0200

    dev: Refactor the RecordableEntitiesSubscriber
    I started to work on a big refactoring, moving all I can in the onFlush
    event handler. However, the code wasn't so good. Worse, I realized it
    didn't even solve my problem of many-to-many relations changes not being
    correctly recorded (but see comment in the processEntityCollectionUpdates
    method, and the next commit).
    
    However I learnt a lot during the process. I also kept what I think is
    better than before (e.g. moving deletion events into the onFlush event
    handler).

--- src/ActivityMonitor/RecordableEntitiesSubscriber.php
+++ src/ActivityMonitor/RecordableEntitiesSubscriber.php
@@ -6,14 +6,14 @@
 
 namespace App\ActivityMonitor;
 
-use App\Entity\EntityEvent;
-use App\Repository\EntityEventRepository;
+use App\Entity;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Events;
 use Doctrine\ORM\PersistentCollection;
 use Doctrine\ORM\UnitOfWork;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
-use Doctrine\Persistence\ObjectManager;
 
 /**
  * Monitor the insertions, updates and deletions of entities and save them as
@@ -22,40 +22,33 @@ use Doctrine\Persistence\ObjectManager;
  */
 #[AsDoctrineListener(event: Events::postPersist)]
 #[AsDoctrineListener(event: Events::postUpdate)]
-#[AsDoctrineListener(event: Events::preRemove)]
-#[AsDoctrineListener(event: Events::postRemove)]
+#[AsDoctrineListener(event: Events::onFlush)]
 class RecordableEntitiesSubscriber
 {
-    private ?EntityEvent $entityEventRemove = null;
-
-    public function __construct(
-        private EntityEventRepository $entityEventRepository
-    ) {
-    }
-
     /**
      * Save the insertions of the RecordableEntityInterface entities as
      * EntityEvents.
      *
-     * @param LifecycleEventArgs<ObjectManager> $args
+     * @param LifecycleEventArgs<EntityManager> $args
      */
     public function postPersist(LifecycleEventArgs $args): void
     {
         $entity = $args->getObject();
 
-        if (!($entity instanceof RecordableEntityInterface)) {
-            return;
-        }
+        if ($entity instanceof RecordableEntityInterface) {
+            $entityEvent = Entity\EntityEvent::initInsert($entity);
 
-        $entityEvent = EntityEvent::initInsert($entity);
-        $this->entityEventRepository->save($entityEvent, true);
+            $entityManager = $args->getObjectManager();
+            $entityManager->persist($entityEvent);
+            $entityManager->flush();
+        }
     }
 
     /**
      * Save the updates of the RecordableEntityInterface entities as
      * EntityEvents.
      *
-     * @param LifecycleEventArgs<ObjectManager> $args
+     * @param LifecycleEventArgs<EntityManager> $args
      */
     public function postUpdate(LifecycleEventArgs $args): void
     {
@@ -65,7 +58,6 @@ class RecordableEntitiesSubscriber
             return;
         }
 
-        /** @var \Doctrine\ORM\EntityManager $entityManager */
         $entityManager = $args->getObjectManager();
         $unitOfWork = $entityManager->getUnitOfWork();
 
@@ -75,50 +67,44 @@ class RecordableEntitiesSubscriber
         );
 
         if ($entityChanges) {
-            $entityEvent = EntityEvent::initUpdate($entity, $entityChanges);
-            $this->entityEventRepository->save($entityEvent, true);
+            $entityEvent = Entity\EntityEvent::initUpdate($entity, $entityChanges);
+
+            $entityManager->persist($entityEvent);
+            $entityManager->flush();
         }
     }
 
     /**
-     * Store the entity event related to the deletions of the
-     * RecordableEntityInterface entities.
-     *
-     * We need to initialize the EntityEvent in preRemove callback because
-     * the entity id will always be null in postRemove. We don't want to save
-     * the EntityEvent immediately because the deletion can fail (due to
-     * foreign keys in the database for instance).
-     *
-     * @param LifecycleEventArgs<ObjectManager> $args
+     * Save the deletions of the RecordableEntityInterface entities as
+     * EntityEvents.
      */
-    public function preRemove(LifecycleEventArgs $args): void
+    public function onFlush(OnFlushEventArgs $eventArgs): void
     {
-        $entity = $args->getObject();
+        $entityManager = $eventArgs->getObjectManager();
+        $unitOfWork = $entityManager->getUnitOfWork();
 
-        if (!($entity instanceof RecordableEntityInterface)) {
-            return;
+        $entityEvents = [];
+
+        foreach ($unitOfWork->getScheduledEntityDeletions() as $entity) {
+            if (!($entity instanceof RecordableEntityInterface)) {
+                continue;
+            }
+
+            $entityEvents[] = Entity\EntityEvent::initDelete($entity);
         }
 
-        $this->entityEventRemove = EntityEvent::initDelete($entity);
+        // Save all the entity events in the database.
+        foreach ($entityEvents as $entityEvent) {
+            $entityManager->persist($entityEvent);
+            // This is required by Doctrine in the onFlush event. It likely
+            // acts as flushing during a flush :).
+            $classMetadata = $entityManager->getClassMetadata(Entity\EntityEvent::class);
+            $unitOfWork->computeChangeSet($classMetadata, $entityEvent);
+        }
     }
 
     /**
-     * Save the entity event related to a deletion.
-     *
-     * @param LifecycleEventArgs<ObjectManager> $args
-     */
-    public function postRemove(LifecycleEventArgs $args): void
-    {
-        if (!$this->entityEventRemove) {
-            return;
-        }
-
-        $this->entityEventRepository->save($this->entityEventRemove, true);
-        $this->entityEventRemove = null;
-    }
-
-    /**
-     * Return a list of changes.
+     * Return a list of fields changes.
      *
      * The returned array is indexed by the changed fields and the values are
      * the changes. A change is an array containing two elements: the old and
@@ -169,12 +155,15 @@ class RecordableEntitiesSubscriber
      *
      * Important note: this method heavily relies on internal methods of
      * Doctrine. It may break when updating to a patch or minor version of
-     * Doctrine.
+     * Doctrine. However, I wasn't able to find a better solution, except by
+     * recording the changes manually, which became really annoying.
      *
      * @return array<string, array{mixed, mixed}>
      */
-    private function processEntityCollectionUpdates(RecordableEntityInterface $entity, UnitOfWork $unitOfWork): array
-    {
+    private function processEntityCollectionUpdates(
+        RecordableEntityInterface $entity,
+        UnitOfWork $unitOfWork,
+    ): array {
         $processedChanges = [];
 
         $collectionUpdates = $unitOfWork->getScheduledCollectionUpdates();
@@ -199,13 +188,28 @@ class RecordableEntitiesSubscriber
             }
         }
 
+        // Note that we don't iterate over $unitOfWork->getScheduledCollectionDeletions().
+        // In fact, it (seems to) always returns an empty list during Doctrine
+        // postUpdate event.
+        // It does however return collectionDeletions during Doctrine onFlush
+        // event when emptying a many-to-many relation with a Symfony Form
+        // using an EntityType field. Unfortunately, these collections don't
+        // hold the information of what have been removed and so we cannot
+        // build our "update" EntityEvent correctly.
+        //
+        // In fact, to correctly record many-to-many relations, you MUST set
+        // `by_reference: false` when configuring a form field corresponding to
+        // a many-to-many relation (see the observers field of
+        // `\App\Form\Ticket\ActorsForm`). With this, the changes are visible
+        // in the collectionUpdates (either on postUpdate or onFlush events).
+        //
+        // See https://stackoverflow.com/a/15394241
+
         return $processedChanges;
     }
 
     /**
      * Return a stringifiable representation of the given value.
-     *
-     * @return string|int|float|bool|mixed[]|null
      */
     private function processChangesValue(mixed $value): mixed
     {
