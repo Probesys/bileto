569900bf375135045cdb1902537ecf743bdbce3c
Marien Fressinaud <marien.fressinaud@probesys.coop>
2024-10-10 17:46:09 +0200

    Refactor the Ticket StatusForm

---
+++ src/Form/Ticket/StatusForm.php
@@ -0,0 +1,45 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2024 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Form\Ticket;
+
+use App\Entity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatableMessage;
+
+class StatusForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('status', Type\ChoiceType::class, [
+            'choices' => Entity\Ticket::STATUSES,
+            'choice_label' => function (string $choice): TranslatableMessage {
+                return new TranslatableMessage("tickets.status.{$choice}");
+            },
+            'label' => new TranslatableMessage('tickets.status'),
+        ]);
+
+        $builder->add('submit', Type\SubmitType::class, [
+            'label' => new TranslatableMessage('forms.save_changes'),
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Entity\Ticket::class,
+            'csrf_token_id' => 'ticket status',
+            'csrf_message' => 'csrf.invalid',
+            'attr' => [
+                'class' => 'form--standard',
+                'data-turbo-preserve-scroll' => true,
+            ],
+        ]);
+    }
+}
