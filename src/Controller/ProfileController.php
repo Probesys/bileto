a60eeaa5e169c95a911665a743f0fd8d49d36226
Marien Fressinaud <marien.fressinaud@probesys.coop>
2024-10-23 18:09:59 +0200

    Refactor the Profile form

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,120 +6,39 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
-use App\Utils\ConstraintErrorsFormatter;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use App\Entity;
+use App\Form;
+use App\Repository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Translation\TranslatableMessage;
-use Symfony\Contracts\Translation\TranslatorInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProfileController extends BaseController
 {
-    #[Route('/profile', name: 'profile', methods: ['GET', 'HEAD'])]
+    #[Route('/profile', name: 'profile')]
     public function edit(
-        #[Autowire(env: 'bool:LDAP_ENABLED')]
-        bool $ldapEnabled,
-    ): Response {
-        /** @var \App\Entity\User $user */
-        $user = $this->getUser();
-        return $this->render('profile/edit.html.twig', [
-            'name' => $user->getName(),
-            'email' => $user->getEmail(),
-            'managedByLdap' => $ldapEnabled && $user->getAuthType() === 'ldap',
-        ]);
-    }
-
-    #[Route('/profile', name: 'update profile', methods: ['POST'])]
-    public function update(
         Request $request,
-        UserRepository $userRepository,
-        UserPasswordHasherInterface $passwordHasher,
-        ValidatorInterface $validator,
-        RequestStack $requestStack,
-        TranslatorInterface $translator,
-        #[Autowire(env: 'bool:LDAP_ENABLED')]
-        bool $ldapEnabled,
+        Repository\UserRepository $userRepository,
     ): Response {
-        /** @var \App\Entity\User $user */
+        /** @var Entity\User */
         $user = $this->getUser();
 
-        $initialName = $user->getName();
-        $initialEmail = $user->getEmail();
+        $form = $this->createNamedForm('profile', Form\User\ProfileForm::class, $user);
 
-        /** @var string $name */
-        $name = $request->request->get('name', $initialName);
+        $form->handleRequest($request);
 
-        /** @var string $email */
-        $email = $request->request->get('email', $initialEmail);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $userRepository->save($user, true);
 
-        /** @var string $currentPassword */
-        $currentPassword = $request->request->get('currentPassword', '');
+            $this->addFlash('success', new TranslatableMessage('notifications.saved'));
 
-        /** @var string $newPassword */
-        $newPassword = $request->request->get('newPassword', '');
-
-        /** @var string $csrfToken */
-        $csrfToken = $request->request->get('_csrf_token', '');
-
-        $managedByLdap = $ldapEnabled && $user->getAuthType() === 'ldap';
-
-        if ($managedByLdap) {
-            return $this->renderBadRequest('profile/edit.html.twig', [
-                'name' => $name,
-                'email' => $email,
-                'managedByLdap' => $managedByLdap,
-                'error' => $translator->trans('user.ldap.cannot_update_profile', [], 'errors'),
-            ]);
+            return $this->redirectToRoute('profile');
         }
 
-        if (!$this->isCsrfTokenValid('update profile', $csrfToken)) {
-            return $this->renderBadRequest('profile/edit.html.twig', [
-                'name' => $name,
-                'email' => $email,
-                'managedByLdap' => $managedByLdap,
-                'error' => $translator->trans('csrf.invalid', [], 'errors'),
-            ]);
-        }
-
-        if ($newPassword) {
-            if (!$passwordHasher->isPasswordValid($user, $currentPassword)) {
-                return $this->renderBadRequest('profile/edit.html.twig', [
-                    'name' => $name,
-                    'email' => $email,
-                    'managedByLdap' => $managedByLdap,
-                    'errors' => [
-                        'password' => $translator->trans('user.password.dont_match', [], 'errors'),
-                    ],
-                ]);
-            }
-
-            $newHashedPassword = $passwordHasher->hashPassword($user, $newPassword);
-            $user->setPassword($newHashedPassword);
-        }
-
-        $user->setName($name);
-        $user->setEmail($email);
-
-        $errors = $validator->validate($user);
-        if (count($errors) > 0) {
-            $user->setEmail($initialEmail);
-            return $this->renderBadRequest('profile/edit.html.twig', [
-                'name' => $name,
-                'email' => $email,
-                'managedByLdap' => $managedByLdap,
-                'errors' => ConstraintErrorsFormatter::format($errors),
-            ]);
-        }
-
-        $userRepository->save($user, true);
-
-        $this->addFlash('success', new TranslatableMessage('notifications.saved'));
-
-        return $this->redirectToRoute('profile');
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
---
+++ src/Form/User/ProfileForm.php
@@ -0,0 +1,132 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2024 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Form\User;
+
+use App\Entity;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints as SecurityAssert;
+use Symfony\Component\Translation\TranslatableMessage;
+
+class ProfileForm extends AbstractType
+{
+    public function __construct(
+        #[Autowire(env: 'bool:LDAP_ENABLED')]
+        private bool $ldapEnabled,
+    ) {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
+            $form = $event->getForm();
+            $user = $event->getData();
+            $managedByLdap = $this->isManagedByLdap($user);
+
+            $form->add('name', Type\TextType::class, [
+                'empty_data' => '',
+                'required' => false,
+                'trim' => true,
+                'disabled' => $managedByLdap,
+                'label' => new TranslatableMessage('users.name'),
+                'attr' => [
+                    'maxlength' => Entity\User::NAME_MAX_LENGTH,
+                    'autocomplete' => 'name',
+                ],
+            ]);
+
+            $form->add('email', Type\EmailType::class, [
+                'empty_data' => '',
+                'trim' => true,
+                'disabled' => $managedByLdap,
+                'label' => new TranslatableMessage('users.email'),
+                'attr' => [
+                    'autocomplete' => 'email',
+                ],
+            ]);
+
+            if (!$managedByLdap) {
+                $form->add('currentPassword', Type\PasswordType::class, [
+                    'empty_data' => '',
+                    'mapped' => false,
+                    'required' => false,
+                    'label' => new TranslatableMessage('profile.current_password'),
+                    'attr' => [
+                        'autocomplete' => 'current-password',
+                    ],
+                    'constraints' => [
+                        new SecurityAssert\UserPassword(
+                            message: new TranslatableMessage('user.password.dont_match', [], 'errors'),
+                            groups: ['change_password'],
+                        ),
+                    ],
+                ]);
+
+                $form->add('plainPassword', Type\PasswordType::class, [
+                    'empty_data' => '',
+                    'hash_property_path' => 'password',
+                    'mapped' => false,
+                    'required' => false,
+                    'label' => new TranslatableMessage('profile.new_password'),
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ]);
+
+                $form->add('submit', Type\SubmitType::class, [
+                    'label' => new TranslatableMessage('forms.save_changes'),
+                ]);
+            }
+        });
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $user = $form->getData();
+        $view->vars['managedByLdap'] = $this->isManagedByLdap($user);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Entity\User::class,
+            'csrf_token_id' => 'profile',
+            'csrf_message' => 'csrf.invalid',
+            'validation_groups' => function (FormInterface $form): array {
+                $groups = ['Default'];
+
+                if (!$form->has('plainPassword')) {
+                    return $groups;
+                }
+
+                $plainPassword = $form->get('plainPassword')->getData();
+
+                if ($plainPassword) {
+                    $groups[] = 'change_password';
+                }
+
+                return $groups;
+            },
+            'attr' => [
+                'class' => 'form--standard',
+            ],
+        ]);
+    }
+
+    private function isManagedByLdap(Entity\User $user): bool
+    {
+        return $this->ldapEnabled && $user->getAuthType() === 'ldap';
+    }
+}
