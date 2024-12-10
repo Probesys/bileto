52e39f572df174c0b78272b41f35602b05705ca2
Marien Fressinaud <marien.fressinaud@probesys.coop>
2024-12-10 17:41:27 +0100

    imp: Allow to customize the logo in the emails

---
+++ src/Twig/SettingsExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2024 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class SettingsExtension extends AbstractExtension
+{
+    public function __construct(
+        private string $customLogoPathname,
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction(
+                'logo',
+                [$this, 'logo'],
+                ['is_safe' => ['html']]
+            ),
+        ];
+    }
+
+    public function logo(): string
+    {
+        if (file_exists($this->customLogoPathname)) {
+            return '@settings/logo.svg';
+        } else {
+            return '@public/logo.svg';
+        }
+    }
+}
