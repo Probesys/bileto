a7d9cf0870ca88c9a2c3de3e488b3fc765f8d4ec
Marien Fressinaud <marien.fressinaud@probesys.coop>
2024-12-11 10:24:21 +0100

    tec: Make sure folders under var are created in Docker image

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 rm -f /tmp/healthy.txt
 
+mkdir -p var/data
+mkdir -p var/settings
+mkdir -p var/uploads
+
 # Make sure to migrate the application
 php bin/console cache:clear
 php bin/console doctrine:migration:migrate --no-interaction
