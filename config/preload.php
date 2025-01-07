c63525a1e434ce472a7f4fb10bc887e7003b91cf
Marien Fressinaud <marien.fressinaud@probesys.coop>
2025-01-07 16:24:31 +0100

    dev: Update the copyright year

--- config/preload.php
+++ config/preload.php
@@ -1,7 +1,7 @@
 <?php
 
 // This file is part of Bileto.
-// Copyright 2022-2024 Probesys
+// Copyright 2022-2025 Probesys
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
--- src/Entity/Contract.php
+++ src/Entity/Contract.php
@@ -1,7 +1,7 @@
 <?php
 
 // This file is part of Bileto.
-// Copyright 2022-2024 Probesys
+// Copyright 2022-2025 Probesys
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 namespace App\Entity;
