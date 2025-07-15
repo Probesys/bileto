dc78f8debe094786315166cf1780e8489b7dc91f
Marien Fressinaud <marien.fressinaud@probesys.coop>
2025-07-15 15:11:34 +0200

    WIP

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -12,6 +12,16 @@ php bin/console cache:clear
 php bin/console doctrine:migration:migrate --no-interaction
 php bin/console db:seeds:load
 
+# Configure the trusted proxies to make sure to get the real client IP
+# TODO how to run this without being root? :)
+if [ "$TRUSTED_PROXY" = "" ]; then
+    # Comment the RemoteIPInternalProxy directive
+    sed -i "s/RemoteIPInternalProxy/\t\t# RemoteIPInternalProxy/" /etc/apache2/sites-available/000-default.conf
+else
+    # Set custom list for RemoteIPInternalProxy
+    sed -i "s/RemoteIPInternalProxy/\t\tRemoteIPInternalProxy $TRUSTED_PROXIES/" /etc/apache2/sites-available/000-default.conf
+fi
+
 touch /tmp/healthy.txt
 
 exec "$@"
