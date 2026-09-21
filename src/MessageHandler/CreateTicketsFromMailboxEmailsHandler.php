Probesys/bileto

commit 2823fc380e55e5c24d02f963f7fe288c5a614795

sec: Limit unattached documents to organizations
When a user uploaded a file as a MessageDocument, it was first marked as
"unattached", waiting to be attached to the next message to be posted.

If a user uploaded a file while creating a ticket or answering to one,
then changed of ticket before submitting the form, the uploaded file was
still in the form of the other organization. While the file is visible,
a distracted user could just miss it and complete the form without
removing it. This could lead to leak sensitive file from a customer to
another.

This change marks the unattached MessageDocuments in a certain context.
The context is either the current organization (new ticket) or the
current ticket (new answer), so the uploaded document is always marked
for a single form.

Then, the MessageDocumentsSubscriber calculates the correct context
depending on if the ticket was created right now (so it must use the
organization context) or if it was already existing (using the ticket
context then).

src/TicketActivity/MessageDocumentsSubscriber.php
@@ -29,10 +29,29 @@ class MessageDocumentsSubscriber implements EventSubscriberInterface
     public function attachMessageDocuments(MessageEvent $event): void
     {
         $message = $event->getMessage();
+        $ticket = $message->getTicket();
+
+        // We consider a ticket to be new if it and the message were both
+        // created within the same time window (arbitrarily set to 2 seconds
+        // which should be way enough).
+        $ticketCreatedAt = $ticket->getCreatedAt()->getTimestamp();
+        $messageCreatedAt = $message->getCreatedAt()->getTimestamp();
+        $isNewTicket = abs($ticketCreatedAt - $messageCreatedAt) <= 2;
+
+        $context = "ticket-{$ticket->getUid()}";
+
+        if ($isNewTicket) {
+            // The ticket has just been created: we must use the context of the
+            // organization as the ticket UID wasn't known when uploading the
+            // files.
+            $organization = $ticket->getOrganization();
+            $context = "organization-{$organization->getUid()}";
+        }
 
         // Fetch all the unattached message documents of the author of the message.
         $messageDocuments = $this->messageDocumentRepository->findBy([
             'createdBy' => $message->getCreatedBy(),
+            'context' => $context,
             'message' => null,
         ]);
 

migrations/Version20260903150455AddContextToMessageDocument.php
@@ -1,0 +1,43 @@
+<?php
+
+// This file is part of Bileto.
+// Copyright 2022-2026 Probesys
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Platforms\MariaDBPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+// phpcs:disable Generic.Files.LineLength
+final class Version20260903150455AddContextToMessageDocument extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add context fields to message_document table.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $dbPlatform = $this->connection->getDatabasePlatform();
+        if ($dbPlatform instanceof PostgreSQLPlatform) {
+            $this->addSql("ALTER TABLE message_document ADD context VARCHAR(255) DEFAULT '' NOT NULL");
+        } elseif ($dbPlatform instanceof MariaDBPlatform) {
+            $this->addSql("ALTER TABLE message_document ADD context VARCHAR(255) DEFAULT '' NOT NULL");
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        $dbPlatform = $this->connection->getDatabasePlatform();
+        if ($dbPlatform instanceof PostgreSQLPlatform) {
+            $this->addSql('ALTER TABLE message_document DROP context');
+        } elseif ($dbPlatform instanceof MariaDBPlatform) {
+            $this->addSql('ALTER TABLE message_document DROP context');
+        }
+    }
+}

src/Controller/MessageDocumentsController.php
@@ -37,10 +37,10 @@ class MessageDocumentsController extends BaseController
     {
         $this->denyAccessUnlessGranted('orga:see', 'any');
 
+        $context = trim($request->query->getString('context', ''));
         $file = $request->files->get('document');
 
-        /** @var string */
-        $csrfToken = $request->request->get('_csrf_token', '');
+        $csrfToken = $request->request->getString('_csrf_token', '');
 
         if (!$this->isCsrfTokenValid('create message document', $csrfToken)) {
             return new JsonResponse([
@@ -85,6 +85,8 @@ class MessageDocumentsController extends BaseController
                 ], Response::HTTP_INTERNAL_SERVER_ERROR);
             }
         }
+
+        $messageDocument->setContext($context);
 
         $this->messageDocumentRepository->save($messageDocument, true);
 
@@ -243,6 +245,9 @@ class MessageDocumentsController extends BaseController
             $conditions['message'] = null;
         }
 
+        $context = $request->query->get('context', '');
+        $conditions['context'] = $context;
+
         $messageDocuments = $this->messageDocumentRepository->findBy(
             $conditions,
             ['createdAt' => 'ASC'],
@@ -250,6 +255,7 @@ class MessageDocumentsController extends BaseController
 
         return $this->render('message_documents/index.html.twig', [
             'messageDocuments' => $messageDocuments,
+            'context' => $context,
         ]);
     }
 }

src/Entity/MessageDocument.php
@@ -127,6 +127,9 @@ class MessageDocument implements EntityInterface, MonitorableEntityInterface, Ui
     #[ORM\JoinColumn(onDelete: 'CASCADE')]
     private ?Message $message = null;
 
+    #[ORM\Column(length: 255, options: ['default' => ''])]
+    private string $context = '';
+
     public function getName(): ?string
     {
         return $this->name;
@@ -272,6 +275,18 @@ class MessageDocument implements EntityInterface, MonitorableEntityInterface, Ui
     public function setMessage(?Message $message): static
     {
         $this->message = $message;
+
+        return $this;
+    }
+
+    public function getContext(): string
+    {
+        return $this->context;
+    }
+
+    public function setContext(string $context): static
+    {
+        $this->context = $context;
 
         return $this;
     }

src/Form/AnswerForm.php
@@ -124,6 +124,9 @@ class AnswerForm extends AbstractType
                 'trim' => true,
                 'sanitize_html' => true,
                 'sanitizer' => 'app.message_sanitizer',
+                'attr' => [
+                    'data-context' => "ticket-{$ticket->getUid()}",
+                ],
                 'block_prefix' => 'editor',
             ]);
 

src/Form/MessageTemplateForm.php
@@ -44,6 +44,9 @@ class MessageTemplateForm extends AbstractType
             'trim' => true,
             'sanitize_html' => true,
             'sanitizer' => 'app.message_sanitizer',
+            'attr' => [
+                'data-context' => 'template',
+            ],
             'block_prefix' => 'editor',
         ]);
 

src/Form/TicketForm.php
@@ -152,6 +152,9 @@ class TicketForm extends AbstractType
                 'trim' => true,
                 'sanitize_html' => true,
                 'sanitizer' => 'app.message_sanitizer',
+                'attr' => [
+                    'data-context' => "organization-{$organization->getUid()}",
+                ],
                 'block_prefix' => 'editor',
                 'constraints' => [
                     new Assert\NotBlank(

templates/form/bileto_theme.html.twig
@@ -198,7 +198,7 @@
             'required': false,
             'attr': {
                 'data-controller': 'tinymce',
-                'data-tinymce-upload-url-value': path('create message document'),
+                'data-tinymce-upload-url-value': path('create message document', { context: form.vars.attr['data-context'] | default('') }),
                 'data-tinymce-upload-csrf-value': csrf_token('create message document'),
                 'data-editor-target': 'tinymce',
                 'data-action': 'new-document->editor#newDocument remove-document->tinymce#removeImage',
@@ -208,7 +208,7 @@
         <div class="editor__documents">
             <turbo-frame
                 id="message-documents"
-                src="{{ path('message documents', { filter: 'unattached' }) }}"
+                src="{{ path('message documents', { filter: 'unattached', context: form.vars.attr['data-context'] | default('') }) }}"
                 data-controller="message-documents"
                 data-editor-target="messageDocuments"
                 data-action="remove-document->editor#removeDocument new-document->message-documents#reload"

commit 1691adc9e19de451865b23f8d3be5c53d85b8f76

fix(email): Fix attachments not being attached to messages
Bug introduced in 2823fc38

The security patch introduced a "context" to make sure documents were
attached to the correct messages. Unfortunately, I forget to adapt the
code of the CreateTicketsFromMailboxEmailsHandler :/

src/MessageHandler/CreateTicketsFromMailboxEmailsHandler.php
@@ -139,7 +139,7 @@ class CreateTicketsFromMailboxEmailsHandler
 
         // The important part: create the message by using the email
         // information and attach it to the ticket.
-        $message = $this->createMessage($mailboxEmail, $ticket);
+        $message = $this->createMessage($mailboxEmail, $ticket, $isNewTicket);
 
         // Finally, dispatch the different events corresponding to what happened.
         if ($isNewTicket) {
@@ -383,9 +383,16 @@ class CreateTicketsFromMailboxEmailsHandler
     private function createMessage(
         Entity\MailboxEmail $mailboxEmail,
         Entity\Ticket $ticket,
+        bool $isNewTicket,
     ): Entity\Message {
         // Extract the attachments and format the email body correctly.
-        $messageDocuments = $this->storeAttachments($mailboxEmail);
+        if ($isNewTicket) {
+            $organization = $ticket->getOrganization();
+            $context = "organization-{$organization->getUid()}";
+        } else {
+            $context = "ticket-{$ticket->getUid()}";
+        }
+        $messageDocuments = $this->storeAttachments($mailboxEmail, $context);
         $this->messageDocumentRepository->save($messageDocuments, true);
 
         $messageContent = $mailboxEmail->getBody();
@@ -473,7 +480,7 @@ class CreateTicketsFromMailboxEmailsHandler
      *
      * @return array<string, Entity\MessageDocument>
      */
-    private function storeAttachments(Entity\MailboxEmail $mailboxEmail): array
+    private function storeAttachments(Entity\MailboxEmail $mailboxEmail, string $context): array
     {
         $messageDocuments = [];
 
@@ -501,7 +508,9 @@ class CreateTicketsFromMailboxEmailsHandler
             $file = new File($filepath, false);
 
             try {
-                $messageDocuments[$id] = $this->messageDocumentStorage->store($file, $filename);
+                $messageDocument = $this->messageDocumentStorage->store($file, $filename);
+                $messageDocument->setContext($context);
+                $messageDocuments[$id] = $messageDocument;
             } catch (Service\MessageDocumentStorageError $e) {
                 $this->logger->warning(
                     "MailboxEmail #{$mailboxEmail->getId()} cannot import {$filename}: {$e->getMessage()}"

src/TicketActivity/MessageDocumentsSubscriber.php
@@ -32,11 +32,11 @@ class MessageDocumentsSubscriber implements EventSubscriberInterface
         $ticket = $message->getTicket();
 
         // We consider a ticket to be new if it and the message were both
-        // created within the same time window (arbitrarily set to 2 seconds
+        // created within the same time window (arbitrarily set to 5 seconds
         // which should be way enough).
         $ticketCreatedAt = $ticket->getCreatedAt()->getTimestamp();
         $messageCreatedAt = $message->getCreatedAt()->getTimestamp();
-        $isNewTicket = abs($ticketCreatedAt - $messageCreatedAt) <= 2;
+        $isNewTicket = abs($ticketCreatedAt - $messageCreatedAt) <= 5;
 
         $context = "ticket-{$ticket->getUid()}";
 